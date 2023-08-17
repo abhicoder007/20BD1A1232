2087d8863f555511fbf8b73b572550126f6ab969
abhicoder007 <kopulasaiabhishek@gmail.com>
2023-08-17 20:28:18 +0530

    new

---
+++ train/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Train booking website</h1>
+        <h3>number of available seats</h3>
+        <input type="button" name="tarin available" value="1"></input>
+        
+        <h3>book seats </h3>
+        <input type="button" name="tarin available" value="1"></input>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
